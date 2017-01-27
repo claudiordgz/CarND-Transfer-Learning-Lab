ccf3968fbd191468811de7491eb30355ef3e1406
Claudio Rodriguez <claudio.rdgz@gmail.com>
2017-01-26 23:20:30 -0500

    chore: dataset retrieval file and extra ignore rules

---
+++ get.sh
@@ -0,0 +1,5 @@
+#!/bin/bash  
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b432_vgg-100/vgg-100.zip
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b634_resnet-100/resnet-100.zip
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b498_inception-100/inception-100.zip
+find . -name '*.zip' -exec sh -c 'unzip -d "${1%.*}" "$1"' _ {} \;
