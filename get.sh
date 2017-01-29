82e7284acbfad5fcb0935aba4168d2ef4465d9f8
Claudio Rodriguez <claudio.rdgz@gmail.com>
2017-01-28 20:45:03 -0500

    added data to s3 bucket and to get.sh

--- get.sh
+++ get.sh
@@ -2,4 +2,5 @@
 WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b432_vgg-100/vgg-100.zip
 WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b634_resnet-100/resnet-100.zip
 WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b498_inception-100/inception-100.zip
+WGET https://s3.amazonaws.com/claudios.useful.bucket/data/data.zip
 find . -name '*.zip' -exec sh -c 'unzip -d "${1%.*}" "$1"' _ {} \;
